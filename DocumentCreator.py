4a4a093dc000debc5568c06baa6bc533dc6afd75
EricNWP <errogersnwp@gmail.com>
2024-11-28 20:34:18 -0700

    Update DocumentCreator.py

--- DocumentCreator.py
+++ DocumentCreator.py
@@ -1,10 +1,9 @@
 from typing import Type
 from typing import Self
 from abc import ABC, abstractmethod
-from Document import Document
-from WordDocumentCreator import WordDocumentCreator
-from ExcelDocumentCreator import ExcelDocumentCreator
-from PDFDocumentCreator import PDFDocumentCreator
+from WordDocument import WordDocument
+from ExcelDocument import ExcelDocument
+from PDFDocument import PDFDocument
 
 class DocumentCreator:
   def __init__(self) -> None:
