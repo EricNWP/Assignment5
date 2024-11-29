342934ae429e5c83ea70d71788e403cd60317bf2
EricNWP <errogersnwp@gmail.com>
2024-11-28 20:35:39 -0700

    Update DocumentCreator.py

--- DocumentCreator.py
+++ DocumentCreator.py
@@ -1,6 +1,7 @@
 from typing import Type
 from typing import Self
 from abc import ABC, abstractmethod
+from Document import Document
 from WordDocument import WordDocument
 from ExcelDocument import ExcelDocument
 from PDFDocument import PDFDocument
